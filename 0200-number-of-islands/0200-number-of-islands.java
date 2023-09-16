334c613c69161ddd5e2d6f9d32598d17445dbf1d
kyoo0115 <58134412+kyoo0115@users.noreply.github.com>
2023-09-16 23:13:35 +0900

    Time: 31 ms (56.63%), Space: 12.9 MB (55.37%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,25 +1,34 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int count = 0;
-        
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == '1'){
-                    count += 1;
-                    callDFS(grid, i, j);
+public:
+    void dfs(vector<vector<char>>& grid, int y, int x, vector<vector<bool>>& visited){
+    int dy[4] = {-1, 0, 1, 0};
+    int dx[4] = {0, 1, 0, -1};
+    int ny, nx;
+    visited[y][x] = true;
+
+    for(int i = 0; i < 4; i++){
+        ny = dy[i] + y;
+        nx = dx[i] + x;
+        if(ny < 0 || nx < 0 || nx >= grid[0].size() || ny >= grid.size()) continue;
+        if(grid[ny][nx] == '1' && !visited[ny][nx]){
+            dfs(grid, ny, nx, visited);
+        }
+    }
+    return;
+}
+
+int numIslands(vector<vector<char>>& grid) {
+    int count = 0;
+    vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
+
+        for(int i = 0; i < grid.size(); i++) {
+            for(int j = 0; j < grid[i].size(); j++) {
+                if(grid[i][j] == '1' && !visited[i][j]){
+                    dfs(grid, i, j, visited);
+                    count++;
                 }
             }
         }
         return count;
     }
-    public void callDFS(char[][] grid, int i, int j){
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') return;
-        grid[i][j] = '0';
-        callDFS(grid, i + 1, j);
-        callDFS(grid, i - 1, j);
-        callDFS(grid, i, j - 1);
-        callDFS(grid, i, j + 1);
-    }
-}
+};
