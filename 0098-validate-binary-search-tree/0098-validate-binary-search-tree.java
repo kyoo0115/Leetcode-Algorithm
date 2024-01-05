5026ac7b0b0b5dd13b3a85336a08b2ad2ef35669
kyoo0115 <58134412+kyoo0115@users.noreply.github.com>
2024-01-05 18:32:53 +0900

    Time: 0 ms (100.00%), Space: 44.4 MB (20.48%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+
+     private boolean isValidBST(TreeNode node, Integer min, Integer max) {
+        // Base case: an empty tree is a valid BST
+        if (node == null) {
+            return true;
+        }
+
+        // Check the current node's value against the min and max constraints
+        if ((min != null && node.val <= min) || (max != null && node.val >= max)) {
+            return false;
+        }
+
+        // Recursively check the left and right subtrees
+        // Update max for the left subtree and min for the right subtree
+        return isValidBST(node.left, min, node.val) && isValidBST(node.right, node.val, max);
+    }
+}
