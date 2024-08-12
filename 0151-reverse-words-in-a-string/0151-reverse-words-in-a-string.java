c2f3f6a05d57fb3faf063a11126748e701053428
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-08-13 01:31:10 +0900

    Time: 5 ms (92.91%), Space: 43.5 MB (45.48%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static String reverseWords(String s) {
+        String trimmed = s.trim();
+        String removeWhiteSpace = trimmed.replaceAll("\\s+", " ");
+        String[] split = removeWhiteSpace.split(" ");
+
+        int left = 0;
+        int right = split.length - 1;
+        while(left < right) {
+            String temp = split[left];
+            split[left] = split[right];
+            split[right] = temp;
+            left++;
+            right--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String string : split) {
+            sb.append(string).append(" ");
+        }
+
+        if(!sb.isEmpty()) {
+            sb.setLength(sb.length() - 1);
+        }
+
+        return sb.toString();
+    }
+}
