195cbd34b6ad03d0a6ce2c14fac6d110a9c93493
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-10-07 22:50:28 +0800

    Time: 0 ms (100.00%), Space: 42 MB (78.11%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -6,11 +6,9 @@ class Solution {
         
         while (p2 >= 0) {  
             if (p1 >= 0 && nums1[p1] > nums2[p2]) { 
-                nums1[i] = nums1[p1];
-                p1--;
+                nums1[i] = nums1[p1--];
             } else {  
-                nums1[i] = nums2[p2];
-                p2--;
+                nums1[i] = nums2[p2--];
             }
             i--;
         }
