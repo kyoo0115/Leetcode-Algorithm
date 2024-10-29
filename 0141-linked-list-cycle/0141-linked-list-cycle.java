e5b5ba5c1e34c1d6c71208bafa76f3eaef9c864f
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-10-29 15:27:44 +0800

    Time: 0 ms (100.00%), Space: 44 MB (87.42%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,20 +11,20 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        
-        if(head == null) return false;
-        
-        ListNode fast = head.next;
-        ListNode slow = head;
-        
-        while(slow != fast) {
-            
-            if(fast == null || fast.next == null) return false;
-            
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        
+    
+    if(head == null) return false;
+    
+    ListNode slow = head;
+    ListNode fast = head.next;
+
+    while(fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+
+      if(slow == fast) {
         return true;
+      }
     }
+    return false;
+  }
 }
